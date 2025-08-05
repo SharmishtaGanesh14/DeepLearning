19342f0435c034487d8b23e04058328627b1db1e
IBAB Student <ibab@IBAB-MScBDB02-Comp006.ibab.ac.in>
2025-08-05 20:16:41 +0530

    Implementing computational graph/basic code, did not generalise yet

--- Lab3.py
+++ Lab3.py
@@ -1,32 +1,86 @@
-import operator
+class Node:
+    def __init__(self, inputs=None, value=None):
+        self.value = value
+        self.grad = 0.0
+        self.inputs = inputs or []
 
-def perform_operation(i1, i2, op):
-    return op(i1, i2)
+class InputNode(Node):
+    def forward(self, val=None):
+        if val is not None:
+            self.value = val
+        return self.value
 
-def compute_derivative(op, i1, i2):
-    if op == operator.mul:
-        return i2, i1
-    elif op == operator.add:
-        return 1, 1
-    elif op == operator.sub:
-        return 1, -1
-    elif op == operator.truediv:
-        return 1/i2, -i1/(i2**2)
-    else:
-        raise ValueError("Unsupported operation")
+    def backward(self, grad_op):
+        self.grad += grad_op
 
-def node(input,weight,operation):
-    output = perform_operation(input, weight, operation)
-    der1,der2= compute_derivative(operation, input, weight)
-    return output,der1,der2
+class AddNode(Node):
+    def forward(self):
+        self.value = sum(node.forward() for node in self.inputs)
+        return self.value
 
-def run():
-    x1, w1, x2, w2 = 2, 3, 4, 5
-    graph={"Layer1":{operator.mul:[x1,w1],operator.mul:[x2,w2]}}
-    f1,der_f1x1,der_f1w1 = node(x1, w1, operator.mul)
-    f2,der_f2x2,der_f2w2 = node(x2, w2, operator.mul)
-    f3, der_f3f1, der_f3f2 = node(f1, f2, operator.add)
+    def backward(self, grad_op):
+        for node in self.inputs:
+            node.backward(grad_op)
+
+class SubNode(Node):
+    def forward(self):
+        if not self.inputs:
+            self.value = 0.0
+            return self.value
+        diff = self.inputs[0].forward()
+        for node in self.inputs[1:]:
+            diff -= node.forward()
+        self.value = diff
+        return self.value
+
+    def backward(self, grad_op):
+        if not self.inputs:
+            return
+        self.inputs[0].backward(grad_op)
+        for node in self.inputs[1:]:
+            node.backward(-grad_op)
+
+class MulNode(Node):
+    def forward(self):
+        if len(self.inputs) != 2:
+            raise ValueError("MulNode requires exactly 2 inputs")
+        a = self.inputs[0].forward()
+        b = self.inputs[1].forward()
+        self.value = a * b
+        return self.value
+
+    def backward(self, grad_op):
+        if len(self.inputs) != 2:
+            return
+        x, y = self.inputs
+        x.backward(grad_op * y.value)
+        y.backward(grad_op * x.value)
+
+# def reset_gradients(node):
+#     node.grad = 0.0
+#     for inp in node.inputs:
+#         reset_gradients(inp)
 
 def main():
-    run()
-main()
+    x1 = InputNode()
+    w1 = InputNode()
+    x2 = InputNode()
+    w2 = InputNode()
+    mulN = MulNode(inputs=[x1, w1])
+    subN = SubNode(inputs=[x2, w2])
+    addN = AddNode(inputs=[mulN, subN])
+    x1.forward(3.0)
+    w1.forward(2.0)
+    x2.forward(4.0)
+    w2.forward(5.0)
+    res = addN.forward()
+    print(res)
+    # reset_gradients(addN)
+    addN.backward(1.0)
+    print("dz/dx1:", x1.grad)
+    print("dz/dw1:", w1.grad)
+    print("dz/dx2:", x2.grad)
+    print("dz/dw2:", w2.grad)
+
+if __name__ == "__main__":
+    main()
