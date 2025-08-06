3e53c8124f44fbc89608a81adec8960af2c8b1f9
IBAB Student <ibab@IBAB-MScBDB02-Comp006.ibab.ac.in>
2025-08-06 18:30:45 +0530

    Added relu and sigmoid function

--- Lab3.py
+++ Lab3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Node:
     def __init__(self, inputs=None, value=None):
         self.value = value
@@ -56,12 +58,45 @@ class MulNode(Node):
         x.backward(grad_op * y.value)
         y.backward(grad_op * x.value)
 
-# def reset_gradients(node):
-#     node.grad = 0.0
-#     for inp in node.inputs:
-#         reset_gradients(inp)
+
+class ReluNode(Node):
+    def __init__(self, input_node):  # Enforce single input
+        super().__init__(inputs=[input_node])
+
+    def forward(self):
+        input_val = self.inputs[0].forward()  # Call forward on input
+        self.value = max(0, input_val)
+        return self.value
+
+    def backward(self, grad_op):
+        input_node = self.inputs[0]
+        if input_node.value > 0:
+            input_node.backward(grad_op)
+        else:
+            input_node.backward(0)
+
+
+class SigmoidNode(Node):
+    def __init__(self, input_node):  # Enforce single input
+        super().__init__(inputs=[input_node])
+
+    def forward(self):
+        input_val = self.inputs[0].forward()  # Call forward on input
+        self.value = 1 / (1 + np.exp(-input_val))
+        return self.value
+
+    def backward(self, grad_op):
+        input_node = self.inputs[0]
+        sig = self.value
+        input_node.backward(grad_op * sig * (1 - sig))
+
+def reset_gradients(node):
+    node.grad = 0.0
+    for inp in node.inputs:
+        reset_gradients(inp)
 
 def main():
+    # 1)
     x1 = InputNode()
     w1 = InputNode()
     x2 = InputNode()
@@ -81,6 +116,23 @@ def main():
     print("dz/dw1:", w1.grad)
     print("dz/dx2:", x2.grad)
     print("dz/dw2:", w2.grad)
+
+    # 2)
+    input_val = InputNode()
+    reluN = ReluNode(input_val)
+    sigN = SigmoidNode(reluN)
+    input_val.forward(-2.0)
+    print("Sigmoid(ReLU(-2)):", sigN.forward())
+    sigN.backward(1.0)
+    print("d/d input (for -2):", input_val.grad)
+
+    # 3)
+    reset_gradients(sigN)
+    input_val.forward(3.0)
+    print("Sigmoid(ReLU(3)):", sigN.forward())
+    sigN.backward(1.0)
+    print("d/d input (for 3):", input_val.grad)
+
 
 if __name__ == "__main__":
     main()
